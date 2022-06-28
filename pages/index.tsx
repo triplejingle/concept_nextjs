2adf7f8fc694f1731577c32cb743d5f8373c120e
triplejingle <djimarotalahatu@gmail.com>
2022-06-28 19:56:50 +0200

    updated package.json

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,8 @@
-import type { NextPage } from 'next'
 
-const Home: NextPage = () => {
-  return (
-      <h1 className="text-3xl font-bold underline">
-Ik kan helaas niet, omdat ik een webinar heb.
-      </h1>
-  )
+export default function Home() {
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    )
 }
-
-export default Home
