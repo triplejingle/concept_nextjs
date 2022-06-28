2ff7d36691f3a52a96fbca7a6c9a37cc88b17c15
triplejingle <djimarotalahatu@gmail.com>
2022-06-28 20:01:46 +0200

    animate bounce

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
     return (
-        <h1 className="text-3xl font-bold underline">
+        <h1 className="animate-bounce w-6 h-6">
             Hello world!
         </h1>
     )
